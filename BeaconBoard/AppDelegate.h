f369751526b0a6c63157a7fe7d3df32348cfb853
Ste Prescott <mail@ste.me>
2015-01-16 17:12:27 +0000

    Base project

---
+++ BeaconBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconBoard
+//
+//  Created by Ste Prescott on 15/01/2015.
+//  Copyright (c) 2015 Ste Prescott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconBoard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BeaconBoard
+//
+//  Created by Ste Prescott on 15/01/2015.
+//  Copyright (c) 2015 Ste Prescott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
